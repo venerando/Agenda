0dfb8527d586886d7a3d85c4307c5d46893ad4ec
Geovanni Venerando <geovanni_venerando@live.com>
2017-06-27 18:39:51 -0300

    Trabalhando com menu - Substituindo botão salvar para salvar através do menu

--- app/src/main/java/com/geovanni/agenda/FormularioActivity.java
+++ app/src/main/java/com/geovanni/agenda/FormularioActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -17,21 +18,7 @@ public class FormularioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
-
-        //Implementando função no botão
-
-        Button botaoSalvar = (Button) findViewById(R.id.formulario_salvar);
-
-        botaoSalvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(FormularioActivity.this, "Botão Clicado", Toast.LENGTH_LONG).show();
-                //Faz retornar a tela anterior ao salava
-                finish();
-            }
-        });
     }
-
 
     //Dgitar apenas onCreateOptionsMenu
 
@@ -43,5 +30,19 @@ public class FormularioActivity extends AppCompatActivity {
         inflater.inflate(R.menu.menu_formulario, menu);
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+
+    //Salvado com botão de confirmação no menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_formulario_ok:
+                Toast.makeText(FormularioActivity.this, "Botão Clicado", Toast.LENGTH_LONG).show();
+                //Faz retornar a tela anterior ao salava
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
