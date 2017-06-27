2cb6995262b7c1384b96eeaafa33f98aeab7da46
Geovanni Venerando <geovanni_venerando@live.com>
2017-06-26 22:23:55 -0300

    Exibindo mensagem com toast ao clicar no botão salvar do formulário

--- app/src/main/java/com/geovanni/agenda/FormularioActivity.java
+++ app/src/main/java/com/geovanni/agenda/FormularioActivity.java
@@ -2,6 +2,9 @@ package com.geovanni.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FormularioActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class FormularioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        //Implementando função no botão
+
+        Button botaoSalvar = (Button) findViewById(R.id.formulario_salvar);
+
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FormularioActivity.this, "Botão Clicado", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
