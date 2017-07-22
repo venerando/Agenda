da3155ec66c6be334c00039165ab83695ceb9d27
Geovanni Venerando <geovanni_venerando@live.com>
2017-07-21 22:39:22 -0300

    Adicionando mais informações na tela

--- app/src/main/java/com/geovanni/agenda/adapter/AlunosAdapter.java
+++ app/src/main/java/com/geovanni/agenda/adapter/AlunosAdapter.java
@@ -62,7 +62,17 @@ public class AlunosAdapter extends BaseAdapter {
         campoNome.setText(aluno.getNome());
 
         TextView campoTelefone =(TextView) view.findViewById(R.id.item_telefone);
-        campoTelefone.setText(aluno.getTelefone());
+            campoTelefone.setText(aluno.getTelefone());
+
+        TextView campoEndereco = (TextView) view.findViewById(R.id.item_endereco);
+        if (campoEndereco != null){
+            campoEndereco.setText(aluno.getEndereco());
+        }
+
+        TextView campoSite = (TextView) view.findViewById(R.id.item_site);
+        if (campoSite !=null){
+            campoSite.setText(aluno.getSite());
+        }
 
         ImageView campoFoto = (ImageView) view.findViewById(R.id.item_foto);
 
