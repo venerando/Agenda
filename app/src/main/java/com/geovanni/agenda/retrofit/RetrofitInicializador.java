75fef9ce032248a20d3b6f7b9674a1c01ddbd6a5
Geovanni Venerando <geovanni_venerando@live.com>
2017-08-10 17:48:31 -0300

    Adicionado Logging Interceptor

--- app/src/main/java/com/geovanni/agenda/retrofit/RetrofitInicializador.java
+++ app/src/main/java/com/geovanni/agenda/retrofit/RetrofitInicializador.java
@@ -3,6 +3,8 @@ package com.geovanni.agenda.retrofit;
 
 import com.geovanni.agenda.services.AlunoService;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
@@ -15,8 +17,19 @@ public class RetrofitInicializador {
     private final Retrofit retrofit;
 
     public RetrofitInicializador(){
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient.Builder client = new OkHttpClient.Builder();
+
+        client.addInterceptor(interceptor);
+
         retrofit = new Retrofit.Builder().baseUrl("http://192.168.0.30:8080/api/")
-                .addConverterFactory(JacksonConverterFactory.create()).build();
+                .addConverterFactory(JacksonConverterFactory.create())
+                .client(client.build())
+                .build();
     }
 
     public AlunoService getAlunoService() {
